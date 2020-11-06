5a500d64467ade47d522ff329e3c3d73fb4382b5
himanshukdev <himanshuk.dev@gmail.com>
2020-11-07 03:59:04 +0530

    Added helper functions to support generic api response.

--- helpers/apiResponse.js
+++ helpers/apiResponse.js
@@ -5,3 +5,37 @@ exports.notFoundResponse = function (res, msg) {
 	};
 	return res.status(404).json(data);
 };
+
+exports.successResponse = function (res, msg) {
+	var data = {
+		status: 1,
+		message: msg
+	};
+	return res.status(200).json(data);
+};
+
+exports.successResponseWithData = function (res, msg, data) {
+	var resData = {
+		status: 200,
+		message: msg,
+		data: data
+	};
+	return res.status(200).json(resData);
+};
+
+exports.ErrorResponse = function (res, msg) {
+	var data = {
+		status: -1,
+		message: msg,
+	};
+	return res.status(500).json(data);
+};
+
+exports.validationErrorWithData = function (res, msg, data) {
+	var resData = {
+		status: 0,
+		message: msg,
+		data: data
+	};
+	return res.status(400).json(resData);
+};
